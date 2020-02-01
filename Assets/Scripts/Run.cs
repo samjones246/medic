902fbf4847817085c2c7e8fbb323bba0be79faca
Sam McCreery <4602020+mccreery@users.noreply.github.com>
2020-02-01 20:24:24 +0000

    Idle + run animation

---
+++ Assets/Scripts/Run.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Run : MonoBehaviour
+{
+    private Animator animator;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        animator.SetBool("IsRunning", Input.GetAxisRaw("Horizontal") != 0 || Input.GetAxisRaw("Vertical") != 0);
+    }
+}
