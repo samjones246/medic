4af5347add385e6af8a9d0788c994216a435afbf
Sam Jones <sam.jones240699@gmail.com>
2020-02-02 01:16:08 +0000

    build-a-person

---
+++ Assets/bed.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class bed : MonoBehaviour
+{
+    public GameObject occupant;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown("f"))
+        {
+            occupant.transform.Find("LegL").gameObject.SetActive(false);
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+
+        List<string> limbs = new List<string>(new string[]{ "ArmL", "ArmR", "LegL", "LegR" });
+        Debug.Log(collision.gameObject.name);
+        if (limbs.Contains(collision.gameObject.name))
+        {
+            occupant.transform.Find(collision.gameObject.name).gameObject.SetActive(true);
+            collision.gameObject.SetActive(false);
+        }
+        
+    }
+}
