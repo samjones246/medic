2929e7e6a2afb66b1be4b7381d7a4b149cbeec86
Sam McCreery <4602020+mccreery@users.noreply.github.com>
2020-02-02 13:15:05 +0000

    Warning timer for items despawning

--- Assets/Scripts/TimedDespawn.cs
+++ Assets/Scripts/TimedDespawn.cs
@@ -8,6 +8,7 @@ public class TimedDespawn : MonoBehaviour
     private float born;
 
     public float lifetime = 20;
+    public float warningFrac = 0.85f;
 
     // Start is called before the first frame update
     void Start()
@@ -18,13 +19,21 @@ public class TimedDespawn : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        float age = (Time.timeSinceLevelLoad - born) / lifetime;
+
         if (GetComponent<limb>().IsPickedUp)
         {
             born = Time.timeSinceLevelLoad;
         }
-        if (Time.timeSinceLevelLoad - born >= lifetime)
+
+        if (age >= 1f)
         {
             Destroy(gameObject);
+        }
+        else if (age >= warningFrac)
+        {
+            float freq = 1 + (1 - age) / (1 - warningFrac);
+            gameObject.GetComponent<Renderer>().enabled = Mathf.Repeat(Time.time, freq) >= (freq / 2f);
         }
     }
 }
