27e6856d42db87124a7808c8a5b18231e0de5f7e
Sam McCreery <4602020+mccreery@users.noreply.github.com>
2020-02-02 04:22:31 +0000

    Basic main menu

---
+++ Assets/Scripts/StartGame.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StartGame : MonoBehaviour
+{
+    public int sceneBuildIndex;
+
+    public void StartGameF()
+    {
+        SceneManager.LoadSceneAsync(sceneBuildIndex, LoadSceneMode.Single);
+    }
+}
